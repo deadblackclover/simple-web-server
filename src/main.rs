aa48374ea95a8fd2404f6e1c813b9e8890b2ccb6
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-01-12 22:25:48 +0700

    Added body

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use clap::{App, Arg};
+use futures::prelude::*;
 use gotham::handler::assets::FileOptions;
+use gotham::handler::HandlerFuture;
 use gotham::helpers::http::response::create_empty_response;
-use gotham::hyper::{Body, HeaderMap, Method, Response, StatusCode, Uri, Version};
+use gotham::hyper::{body, Body, HeaderMap, Method, StatusCode, Uri, Version};
 use gotham::router::builder::*;
 use gotham::state::{FromState, State};
+use std::pin::Pin;
 
 fn log_request(state: &State) {
     let method = Method::borrow_from(state);
@@ -16,10 +19,20 @@ fn log_request(state: &State) {
     println!("Headers: {:?}", headers);
 }
 
-fn request_handler(state: State) -> (State, Response<Body>) {
+fn request_handler(mut state: State) -> Pin<Box<HandlerFuture>> {
     log_request(&state);
-    let res = create_empty_response(&state, StatusCode::OK);
-    (state, res)
+
+    let f = body::to_bytes(Body::take_from(&mut state)).then(|full_body| match full_body {
+        Ok(valid_body) => {
+            let body_content = String::from_utf8(valid_body.to_vec()).unwrap();
+            println!("Body: {}", body_content);
+            let res = create_empty_response(&state, StatusCode::OK);
+            future::ok((state, res))
+        }
+        Err(e) => future::err((state, e.into())),
+    });
+
+    Box::pin(f.boxed())
 }
 
 fn main() {
